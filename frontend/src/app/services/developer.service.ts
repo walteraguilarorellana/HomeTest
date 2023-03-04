858101774c4f53f27329222e69f1e7e39749d038
walteraguilarorellana <walteraguilarorellana@gmail.com>
2023-03-03 20:31:52 -0600

    introducing navigation with navbar tailwind styled

--- frontend/src/app/services/developer.service.ts
+++ frontend/src/app/services/developer.service.ts
@@ -5,8 +5,16 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
+const url = "https://api.github.com/";
+
 export class DeveloperService {
 
   constructor(private http:HttpClient) { }
+
+  //this function get the query from GuiHub API for all components
+ // getData(info:string):Observable<any>{
+  //    return this.http.get<any>(url+info);
+  //}
 
 }
