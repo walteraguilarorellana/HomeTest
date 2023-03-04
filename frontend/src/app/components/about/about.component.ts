63ede200a8bdbfdc1359095b31b1f625198cef15
walteraguilarorellana <walteraguilarorellana@gmail.com>
2023-03-03 21:18:15 -0600

    enabling cors policy, testing frontend to backend comunication'

--- frontend/src/app/components/about/about.component.ts
+++ frontend/src/app/components/about/about.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DeveloperService } from '../../services/developer.service';
+
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent  {  
+    constructor(private developerService:DeveloperService){}
+    ngOnInit(){
+      console.log('aqui estamos')
+      this.getDeveloper();
+    }
 
+    getDeveloper(){
+      this.developerService.getDeveloper().subscribe({
+        next: (v) => console.log(v),
+        error: (e) => console.error(e),
+        complete: () => console.info('complete') 
+      })
+    }
 }
--- frontend/src/app/services/developer.service.ts
+++ frontend/src/app/services/developer.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { Observable } from 'rxjs';
+import { developer } from '../interfaces/developer';
+const url:string = "https://api.github.com";
 
 @Injectable({
   providedIn: 'root'
 })
-
-const url = "https://api.github.com/";
-
 export class DeveloperService {
 
-  constructor(private http:HttpClient) { }
+    Server_URL:string = 'http://localhost:3000/developer';
+
+    constructor(private http:HttpClient) { }
+
+  //this function return the developer info stored in backend nestjs
+    getDeveloper():Observable<developer[]>{
+      return this.http.get<developer[]>(this.Server_URL);
+    }
 
   //this function get the query from GuiHub API for all components
- // getData(info:string):Observable<any>{
-  //    return this.http.get<any>(url+info);
-  //}
+    getData(info:string):Observable<any>{
+      return this.http.get<any>(`${url}/${info}`);
+    }
 
 }
